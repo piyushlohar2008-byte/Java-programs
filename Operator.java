abef2505d1a45e1beff18169a343c8c1101256d4
piyushlohar2008-byte <piyushlohar2008@gmail.com>
2026-02-16 21:23:48 +0530

    Add files via upload

---
+++ Operator.java
@@ -0,0 +1,12 @@
+package operation;
+
+import java.util.*;
+
+public class Operator {
+    
+    public static void main(String[] args) {
+        Date d = new Date();
+            System.out.println(d);
+            System.out.flush();
+    }
+}
