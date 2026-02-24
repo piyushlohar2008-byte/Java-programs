41cf73c2fac58611817fb7f885ed8c0801189ccb
piyushlohar2008-byte <piyushlohar2008@gmail.com>
2026-02-24 17:59:55 +0530

    Delete sample.java

--- sample.java
+++
@@ -1,8 +0,0 @@
-public class sample {
-    
-    public static void main(String[] args) {
-        for (int i = 1; i <= 100; i++) {
-            System.out.println(i+" Hello World");
-        }
-    }
-}
